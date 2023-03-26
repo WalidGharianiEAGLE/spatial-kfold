25004e67bc4f86118c50cd283c567a4dad096aa5
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-03-26 23:17:10 +0200

    updated the setup file

--- setup.py
+++ setup.py
@@ -17,19 +17,30 @@ setup(
     packages=["spatialkfold"],
     author="Walid Ghariani",
     author_email="walid11ghariani@gmail.com",
-    description=("Spatial resampling for cross-validation in python"),
+    description=("spatial-kFold: A Python Package for Spatial Resampling Toward More Reliable Cross-Validation in Spatial Studies."),
     long_description=README,
     license="MIT",
     keywords="cross-validation, machine-learning, GIS, spatial",
     url="https://github.com/WalidGharianiEAGLE/spatial-kfold",
 
-    packages=find_packages(),
-    package_data={'spatialkfold': ['data/*.geojson']},
+    #packages=find_packages(),
+    package_data={'geopoz': ['./data/*.geojson']},
     include_package_data=True,
 
     # Dependencies
     install_requires = requirements,
     python_requires='>=3.7',
+ 
+    # Classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 
     # testing
     setup_requires=['pytest-runner'],
--- test.py
+++
@@ -1,5 +0,0 @@
-# Dependencies
-with open('requirements.txt') as f:
-    requirements = f.readlines()
-
-print(requirements)
