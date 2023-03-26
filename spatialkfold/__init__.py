31e91874d9940953c452753be1ecc25a6e937692
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-03-26 23:12:48 +0200

    added init

---
+++ spatialkfold/__init__.py
@@ -0,0 +1,5 @@
+from .blocks import create_grid, spatial_blocks, spatial_kfold_blocks
+from .clusters import spatial_kfold_clusters
+from .plotting import spatial_kfold_plot, spatial_kfold_autoplot
+from .stats import spatial_kfold_stats
+from .dataloader import load_data
