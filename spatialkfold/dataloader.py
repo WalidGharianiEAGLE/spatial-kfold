01d10d6521eefbc5c3973dd05cd1c298985301b7
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-03-26 23:13:14 +0200

    added dataloader

---
+++ spatialkfold/dataloader.py
@@ -0,0 +1,9 @@
+import geopandas as gpd
+import pkg_resources
+
+def load_data():
+    # Load ames.geojson file
+    filepath = pkg_resources.resource_filename(__name__, 'data/ames.geojson')
+    data = gpd.read_file(filepath)
+    
+    return data
