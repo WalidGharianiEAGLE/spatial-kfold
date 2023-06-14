2ad3d71ab03bb2cd5993b839bd50ae93b92c3b57
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-06-14 11:35:46 +0200

    removed spatial_kfold_blocks function

--- spatialkfold/__init__.py
+++ spatialkfold/__init__.py
@@ -1,4 +1,4 @@
-from .blocks import create_grid, spatial_blocks, spatial_kfold_blocks
+from .blocks import create_grid, spatial_blocks
 from .clusters import spatial_kfold_clusters
 from .plotting import spatial_kfold_plot
 from .stats import spatial_kfold_stats
