a29d25271297d7d43b251c7324f685981888a25e
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-06-14 15:29:11 +0200

    changed loading function-filename

--- spatialkfold/datasets.py
+++ spatialkfold/datasets.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 import pkg_resources
 
-def load_data():
+def load_ames():
     # Load ames.geojson file
     filepath = pkg_resources.resource_filename(__name__, 'data/ames.geojson')
     data = gpd.read_file(filepath)
