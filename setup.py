21691f421584405cabf6b5a849124496d1a8c902
WalidGharianiEAGLE <walid11ghariani@gmail.com>
2023-10-26 10:39:15 +0200

    Update setup.py
    Seted the markdown as the content_type.

--- setup.py
+++ setup.py
@@ -1,29 +1,25 @@
 import pathlib
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# The directory containing this file
 dir = pathlib.Path(__file__).parent
-
-# README file
 README = (dir / "README.md").read_text()
-
 # Dependencies
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
 setup(
     name='spatial-kfold',
-    version="0.0.02",
+    version="0.0.03",
     packages=["spatialkfold"],
     author="Walid Ghariani",
     author_email="walid11ghariani@gmail.com",
     description=("spatial-kfold: A Python Package for Spatial Resampling Toward More Reliable Cross-Validation in Spatial Studies."),
     long_description=README,
+    long_description_content_type="text/markdown", 
     license="GPL-3.0",
     keywords="cross-validation, machine-learning, GIS, spatial",
     url="https://github.com/WalidGharianiEAGLE/spatial-kfold",
 
-    #packages=find_packages(),
     package_data={'spatialkfold': ['./data/*.geojson']},
     include_package_data=True,
 
